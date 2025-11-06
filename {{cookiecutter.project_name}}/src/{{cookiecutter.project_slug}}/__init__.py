fdb15869ffe299c0c23a91ed38ad33fe5a922fe5
Niko Pikall <nikolas.pikall@gmail.com>
2025-11-06 21:36:06 +0100

    revert: removed main cli entrypoint

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -5,8 +5,3 @@ import importlib.metadata
 __version__ = importlib.metadata.version(__name__)
 
 NAME = "{{ cookiecutter.project_slug }}"
-
-
-def main(name: str = NAME) -> None:
-    """Entrypoint for the CLI."""
-    print(f"Hello from {name}")
