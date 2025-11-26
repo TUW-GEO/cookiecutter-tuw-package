257dfef1661ca4f27cce3b97a99b0799607e91b0
npikall <nikolas.pikall@gmail.com>
2025-11-26 16:57:49 +0100

    feat: added greeting function
    - has been added to generate coverage data from the start

--- {{cookiecutter.project_name}}/tests/test_meta.py
+++ {{cookiecutter.project_name}}/tests/test_meta.py
@@ -1,2 +1,12 @@
+from {{ cookiecutter.project_slug }} import greet
+
+
 def test_that_testing_package_is_working():
     assert True
+
+
+def test_greet_returns_correctly():
+    got = greet()
+    want = "Hello from {{ cookiecutter.project_slug }}"
+
+    assert got == want
